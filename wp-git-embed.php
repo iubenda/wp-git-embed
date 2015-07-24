7900200b6b4c96c556fd8e8ba25aad2fc40993bf
Andrea Giannangelo <facens@gmail.com>
2015-07-24 17:44:06 +0200

    Update wp-git-embed.php

--- wp-git-embed.php
+++ wp-git-embed.php
@@ -127,7 +127,7 @@ if(!class_exists('WP_Git_Embed')) {
         //$raw = preg_replace("\t", "  ", $raw);
         $raw = htmlentities($raw);
         //$raw = str_replace(" ", "&nbsp;", $_POST['raw']);
-        //$raw = str_replace("\t", "&nbsp;&nbsp;", $_POST['raw']);
+        $raw = str_replace("\t", "  ", $raw);
 
         if(!empty($format)) {
           if(preg_match('/^pre.*/', $format)) {
